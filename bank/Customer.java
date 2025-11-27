67325d4f919c54f7a35f1b86da3455c787bc150b
arererra <doidietcop02@gmail.com>
2025-11-27 17:07:21 -0500

    Update Customer.java

--- bank/Customer.java
+++ bank/Customer.java
@@ -1,6 +1,7 @@
 public class Customer {
 
     private String name;
+ private Permissions permissions;
 
     public Customer(String name) {
         this.name = name;
@@ -18,4 +19,12 @@ public class Customer {
         System.out.println("Customer info:");
         System.out.println("Name: " + name);
     }
+       public Permissions getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Permissions permissions) {
+        this.permissions = permissions;
+    }
+
 }
