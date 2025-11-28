0bf947a067d47fc8605a226a2020e833d37bbf6b
osarayrah <sarayrah.omar22@gmail.com>
2025-11-27 21:47:56 -0500

    Update DataStore.java

--- bank/DataStore.java
+++ bank/DataStore.java
@@ -1,45 +1,35 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * Test-only storage used for bottom-up integration tests.
+ * (As described in the Integration Testing section of the report.)
+ * 
+ * Real system uses a centralized external database.
+ */
 public class DataStore {
 
-    private final Map<String, User> usersById = new HashMap<>();
-    private final Map<String, Account> accountsByNumber = new HashMap<>();
+    private final List<Account> accounts = new ArrayList<>();
     private final List<Transaction> transactions = new ArrayList<>();
 
-    public void saveUser(User user) {
-        usersById.put(user.getId(), user);
-        for (Account a : user.getAccounts()) {
-            if (a.getAccountNumber() != null)
-                accountsByNumber.put(a.getAccountNumber(), a);
+    public void addAccount(Account account) {
+        if (account != null) {
+            accounts.add(account);
         }
     }
 
-    public void saveAccount(Account account) {
-        if (account.getAccountNumber() != null)
-            accountsByNumber.put(account.getAccountNumber(), account);
+    public void addTransaction(Transaction transaction) {
+        if (transaction != null) {
+            transactions.add(transaction);
+        }
     }
 
-    public void saveTransaction(Transaction tx) {
-        transactions.add(tx);
+    public List<Account> getAccounts() {
+        return Collections.unmodifiableList(accounts);
     }
 
-    public Optional<User> findUserById(String id) {
-        return Optional.ofNullable(usersById.get(id));
-    }
-
-    public Collection<User> getAllUsers() {
-        return Collections.unmodifiableCollection(usersById.values());
-    }
-
-    public Optional<Account> findAccountByNumber(String accNumber) {
-        return Optional.ofNullable(accountsByNumber.get(accNumber));
-    }
-
-    public Collection<Account> getAllAccounts() {
-        return Collections.unmodifiableCollection(accountsByNumber.values());
-    }
-
-    public List<Transaction> getAllTransactions() {
+    public List<Transaction> getTransactions() {
         return Collections.unmodifiableList(transactions);
     }
 }
