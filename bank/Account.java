c6d58f97ac694bf116bba63d3b5739c4b3b08214
osarayrah <sarayrah.omar22@gmail.com>
2025-11-27 20:07:56 -0500

    Refactor Account class to simplify user handling
    Refactor Account class to use Customer instead of List<User> and remove Permissions. Update constructors and methods accordingly.

--- bank/Account.java
+++ bank/Account.java
@@ -3,46 +3,37 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Account entity as defined in MyBankUML report.
- * Holds financial data only: balance, account number,
- * account type, owners, permissions, and transaction history.
+ * Abstract base for all account types (Card, Check, Saving).
+ * Holds balance and transaction history.
  */
 public abstract class Account {
 
-    // Required by Component Level Design (Page 27)
-    protected Permissions perms;
-    protected List<User> users;
+    protected Customer customer;
+    protected List<Transaction> transactions;
 
-    // Core financial attributes
+    // Design attributes from document
     protected String accountNumber;
     protected String accountType;
     protected double balance;
 
-    protected List<Transaction> transactions;
+    public Account(Customer customer) {
+        this(customer, null, null, 0.0);
+    }
 
-    public Account(List<User> users,
-                   String accountNumber,
-                   String accountType,
-                   double openingBalance,
-                   Permissions perms) {
-
-        this.users = (users != null) ? users : new ArrayList<>();
+    public Account(Customer customer, String accountNumber, String accountType, double openingBalance) {
+        this.customer = customer;
+        this.transactions = new ArrayList<>();
         this.accountNumber = accountNumber;
         this.accountType = accountType;
         this.balance = openingBalance;
-        this.perms = perms;
-
-        this.transactions = new ArrayList<>();
     }
 
-    // --- Getters ---
-
-    public List<User> getUsers() {
-        return Collections.unmodifiableList(users);
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public Permissions getPermissions() {
-        return perms;
+    public List<Transaction> getTransactions() {
+        return Collections.unmodifiableList(transactions);
     }
 
     public String getAccountNumber() {
@@ -57,12 +48,6 @@ public abstract class Account {
         return balance;
     }
 
-    public List<Transaction> getTransactions() {
-        return Collections.unmodifiableList(transactions);
-    }
-
-    // --- Setters ---
-
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
@@ -71,16 +56,23 @@ public abstract class Account {
         this.accountType = accountType;
     }
 
-    public void setPermissions(Permissions perms) {
-        this.perms = perms;
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
-    // --- Internal balance update (used by Transaction service only) ---
+    public void addTransaction(Transaction t) {
+        if (t != null) {
+            transactions.add(t);
+        }
+    }
+
+    /**
+     * Used from Transactions service to update balance.
+     */
     protected void applyAmount(double delta) {
         this.balance += delta;
     }
 
-    public void addTransaction(Transaction t) {
-        if (t != null) transactions.add(t);
-    }
+    public abstract void pay();
+    public abstract void receipt();
 }
